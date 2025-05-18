a44ed7414d911ff7f41f82cee7caacef8c64feba
Patrick Pascua <patrick.pascua78@gmail.com>
2025-05-17 23:31:56 -0600

    elipse graph

--- patrick Review.py
+++ patrick Review.py
@@ -18,6 +18,10 @@ new_dr = os.getcwd() ;print(f'new dr: {new_dr}')
 
 
 
+
+
+#%%
+
 # making elipse
 a = np.array([
     [2, 0],
@@ -39,7 +43,6 @@ d = np.array([
     [3, np.sqrt(2)],
     [np.sqrt(2), 3]
 ])
-
 
 def making_ellipse(matrix):
     # Finding eiganval and eiganvector
@@ -77,4 +80,92 @@ for i in range(len(all_circle)):
 plt.legend(loc= 'upper right')
 plt.grid()
 plt.show()
+
+#%%
+
+# Step 2: Define a symmetric positive-definite matrix A (e.g., covariance matrix)
+A = np.array([[3, 1, 1],
+              [1, 2, 1],
+              [1, 1, 1]])
+
+B = np.array([
+    [2, -1, 0],
+    [-1, 2, -1],
+    [0, -1, 2]
+])
+
+C = np.array([
+    [6, 2, 1],
+    [2, 5, 2],
+    [1, 2, 4]
+])
+
+def three_by_three(matrix):
+    #Create a linear interporlation of phi (from 0 to pi) and theta (from 0 to 2pi) with 10 points
+    phi_vals = np.linspace(0, np.pi, 20) # UP and down 
+    theta_vals = np.linspace(0, 2*np.pi, 20) # Full Rotation around z-axis
+    
+    # This creates a grid of coordinate to create the spear
+    # This is done by generating a lattice of all (phi,theta) generated
+    pair_wise = np.meshgrid(phi_vals, theta_vals)
+    
+    # This separates the lattice points into phi and theta
+    phi= pair_wise[0] #(Nort to South)
+    theta = pair_wise[1] #(Rotations)
+    
+    # Creating points to plot in a 3D
+    r1 = 1 # centered at this point
+    x = r1* np.sin(phi) * np.cos(theta)
+    y = r1* np.sin(phi) * np.sin(theta)
+    z = r1* np.cos(phi)
+    
+    sphere = np.vstack((x.flatten(),y.flatten(),z.flatten())) # a faster way to do this is by using np.ravel()
+    
+    
+    # Rotating using eigenvalues and vectors
+    eigans = np.linalg.eig(matrix) # Calculates the eigen values and vectors
+    eigan_val =  eigans[0]
+    eigan_vec =  eigans[1]
+    
+    # Scaling 
+    scal = np.sqrt(eigan_val)
+    diagonalization = np.diag(scal)
+    # Transforming spear into an ellipse using eiganvalues and eigan vectors of matrix
+    tranform = np.matmul(eigan_vec,diagonalization)
+    
+    # transformed spear
+    new_sphere = np.matmul(tranform, sphere)
+    # New sphear coordinate
+    x_trans = new_sphere[0,:].reshape(x.shape)
+    y_trans = new_sphere[1,:].reshape(y.shape)
+    z_trans = new_sphere[2,:].reshape(z.shape)
+    
+    return [x_trans,y_trans,z_trans]
+    
+
+
+
+
+colors = [('navy','blue',0.5,"A"),('lightcoral','red',0.3,"B"), ('seagreen','green',0.1,"C") ]
+coordinates = three_by_three(A)
+coordiantes2 = three_by_three(B)
+coordinate3 = three_by_three(C)
+
+matrix = [coordinates,coordiantes2, coordinate3 ]
+
+fig = plt.figure(figsize=(7, 7))
+ax = fig.add_subplot(1,1,1, projection='3d')
+for i in range(len(matrix)):
+    x,y,z = matrix[i][0],matrix[i][1],matrix[i][2]   
+   
+    ax.plot_surface(x,y,z, color=colors[i][0],  edgecolor = colors[i][1],  alpha = colors[i][2], label = f'matrix: {colors[i][3]}' )
+ax.set_box_aspect([1, 1, 1])
+ax.set_title('Ellipse')
+ax.set_xlabel('X-axis')
+ax.set_ylabel('Y-axis')  
+plt.legend()
+plt.show()
+
+
+
 
